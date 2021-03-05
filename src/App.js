b789015f3f2f2667b56db77f14d2caef14c0ee45
ragavanRagav <ragavan111999@gmail.com>
2021-03-05 23:24:04 +0530

    simple configs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
-
-function App() {
+import EcoTwoToneIcon from '@material-ui/icons/EcoTwoTone';function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Virtual Event
+        Virtual Event<span><EcoTwoToneIcon /></span>
       </header>
     </div>
   );
